a4aae86e4c4c0be1eb7f3f5830e7d9a930b5bd92
Ryan Fung <381273+gruffins@users.noreply.github.com>
2022-12-25 12:52:28 -0500

    Rename repo (#3)

--- BirchXCGLogger.podspec
+++ BirchXCGLogger.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'BirchXCGLogger'
-  s.version = '1.0.1'
+  s.version = '1.0.2'
   s.summary = 'XCGLogger wrapper for Birch.'
   s.homepage = 'https://github.com/gruffins/birch-xcglogger'
   s.license = { type: 'MIT', file: 'LICENSE' }
--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
             targets: ["BirchXCGLogger"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/gruffins/birch-ios.git", .upToNextMajor(from: "1.2.1")),
+        .package(url: "https://github.com/gruffins/birch-swift.git", .upToNextMajor(from: "1.2.3")),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger", .upToNextMajor(from: "7.0.0"))
     ],
     targets: [
         .target(
             name: "BirchXCGLogger",
             dependencies: [
-                .product(name: "Birch", package: "birch-ios"),
+                .product(name: "Birch", package: "birch-swift"),
                 .product(name: "XCGLogger", package: "XCGLogger")
             ]
         )
